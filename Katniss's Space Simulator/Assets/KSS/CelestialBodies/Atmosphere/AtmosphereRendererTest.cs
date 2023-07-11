6220c29dc3b41eb0dc2f159cb4a1898acd27e318
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2023-07-11 20:49:13 +0200

    more fixes, until a proper compositor is added

--- Katniss's Space Simulator/Assets/KSS/CelestialBodies/Atmosphere/AtmosphereRendererTest.cs
+++ Katniss's Space Simulator/Assets/KSS/CelestialBodies/Atmosphere/AtmosphereRendererTest.cs
@@ -1,3 +1,4 @@
+using KSS.Core.ReferenceFrames;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,29 +6,55 @@ using UnityEngine;
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class AtmosphereRendererTest : MonoBehaviour
 {
-    public Material mat;
+    public Shader atmosphereShader;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Material _mat;
+
+    void OnReferenceFrameSwitch( SceneReferenceFrameManager.ReferenceFrameSwitchData data )
     {
-        
+        int id = Shader.PropertyToID( "_Center" );
+        Vector3 oldSceneCenter = _mat.GetVector( id );
+
+        Vector3Dbl oldAirfPos = data.OldFrame.TransformPosition( oldSceneCenter );
+
+        _mat.SetVector( id, data.NewFrame.InverseTransformPosition( oldAirfPos ) );
     }
 
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        _mat = new Material( atmosphereShader );
+        _mat.SetVector( Shader.PropertyToID( "_Center" ), new Vector3( 0, 0, 0 ) );
+        _mat.SetVector( Shader.PropertyToID( "_SunDirection" ), new Vector3( 1, 0, 1 ) );
+        _mat.SetVector( Shader.PropertyToID( "_ScatteringWavelengths" ), new Vector3( 675, 530, 400 ) );
+        _mat.SetFloat( Shader.PropertyToID( "_ScatteringStrength" ), 128 );
+        _mat.SetFloat( Shader.PropertyToID( "_TerminatorFalloff" ), 32 );
+        _mat.SetFloat( Shader.PropertyToID( "_MinRadius" ), 6371000f );
+        _mat.SetFloat( Shader.PropertyToID( "_MaxRadius" ), 6500000f );
+        _mat.SetFloat( Shader.PropertyToID( "_InScatteringPointCount" ), 16 );
+        _mat.SetFloat( Shader.PropertyToID( "_OpticalDepthPointCount" ), 8 );
+        _mat.SetFloat( Shader.PropertyToID( "_DensityFalloffPower" ), 13.7f );
+    }
+
+    private void OnEnable()
+    {
+        SceneReferenceFrameManager.OnAfterReferenceFrameSwitch += OnReferenceFrameSwitch;
+    }
+
+    private void OnDisable()
+    {
+        SceneReferenceFrameManager.OnAfterReferenceFrameSwitch -= OnReferenceFrameSwitch;
     }
 
     [ImageEffectOpaque]
     void OnRenderImage( RenderTexture source, RenderTexture destination )
     {
-        if( mat == null )
+        if( _mat == null )
         {
             Graphics.Blit( source, destination );
             return;
         }
 
-        Graphics.Blit( source, destination, mat );
+        Graphics.Blit( source, destination, _mat );
     }
 }
