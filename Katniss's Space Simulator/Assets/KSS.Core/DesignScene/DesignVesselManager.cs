7192c34dba3e0ea93f2b99689d921fabd1c1252a
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2023-10-29 11:20:35 +0100

    x

---
+++ Katniss's Space Simulator/Assets/KSS.Core/DesignScene/DesignVesselManager.cs
@@ -0,0 +1,42 @@
+﻿using KSS.Core.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityPlus.Serialization.Strategies;
+
+namespace KSS.Core.DesignScene
+{
+    public class DesignVesselManager : SingletonMonoBehaviour<DesignVesselManager>
+    {
+        static JsonExplicitHierarchyGameObjectsStrategy _vesselStrategy = new JsonExplicitHierarchyGameObjectsStrategy( GetGameObjects );
+
+        /// <summary>
+        /// Modify this to point at a different craft file.
+        /// </summary>
+        public static VesselMetadata Metadata { get; set; }
+
+        private static IPartObject _vessel;
+
+        // undos stored in files, preserved across sessions?
+
+        public static void SaveVessel()
+        {
+            // save current vessel to the files defined by metadata's ID.
+        }
+
+        public static void LoadVessel()
+        {
+            // load current vessel from the files defined by metadata's ID.
+        }
+
+        // ------
+
+        private static GameObject[] GetGameObjects()
+        {
+            return new GameObject[] { _vessel.gameObject };
+        }
+    }
+}
---
+++ Katniss's Space Simulator/Assets/_UnityEngine/SingletonMonoBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnityEngine
+{
+    public class SingletonMonoBehaviour<T> : MonoBehaviour where T : SingletonMonoBehaviour<T>
+    {
+        private static T __instance;
+
+        protected static T instance
+        {
+            get
+            {
+                if( __instance == null )
+                {
+                    __instance = FindObjectOfType<T>();
+                }
+                return __instance;
+            }
+        }
+    }
+}
