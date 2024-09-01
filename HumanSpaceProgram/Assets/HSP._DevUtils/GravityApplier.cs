51d5a2703a38ddc54d0102a97fedd755d4bda7ed
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2024-09-01 21:16:42 +0200

    fixed gravity applier being added to a vessel outside the gameplay scene

--- HumanSpaceProgram/Assets/HSP._DevUtils/GravityApplier.cs
+++ HumanSpaceProgram/Assets/HSP._DevUtils/GravityApplier.cs
@@ -1,4 +1,6 @@
 ﻿using HSP.CelestialBodies;
+using HSP.SceneManagement;
+using HSP.Vanilla.Scenes.GameplayScene;
 using HSP.Vessels;
 using UnityEngine;
 
@@ -12,14 +14,17 @@ namespace HSP._DevUtils
         void FixedUpdate()
         {
             Vector3Dbl airfGravityForce = GravityUtils.GetNBodyGravityForce( Vessel.ReferenceFrameTransform.AbsolutePosition, Vessel.PhysicsTransform.Mass );
-           // Vessel.PhysicsTransform.AddForce( (Vector3)airfGravityForce );
+            Vessel.PhysicsTransform.AddForce( (Vector3)airfGravityForce );
         }
 
         [HSPEventListener( HSPEvent_ON_VESSEL_CREATED.ID, "add_gravity_applier" )]
         public static void AddGravityApplier( Vessel vessel )
         {
-            GravityApplier g = vessel.gameObject.AddComponent<GravityApplier>();
-            g.Vessel = vessel;
+            if( SceneLoader.IsSceneLoaded( GameplaySceneManager.SCENE_NAME ) )
+            {
+                GravityApplier g = vessel.gameObject.AddComponent<GravityApplier>();
+                g.Vessel = vessel;
+            }
         }
     }
 }
