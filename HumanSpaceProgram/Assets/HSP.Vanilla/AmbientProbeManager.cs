b50537fecc3e9b9a431c27531f7bf66506b0d403
Katniss218 <52105856+Katniss218@users.noreply.github.com>
2025-10-04 11:37:02 +0200

    WIP ambient reflection/light probe baker

---
+++ HumanSpaceProgram/Assets/HSP.Vanilla/AmbientProbeManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace HSP.Vanilla
+{
+    public class AmbientProbeManager : SingletonMonoBehaviour<AmbientProbeManager>
+    {
+        public ReflectionProbe probe;
+        public RenderTexture targetRT; // cubemap RenderTexture (use RenderTexture.dimension = TextureDimension.Cube) public bool assignToGlobalReflection = true; public bool updateAmbientFromProbe = true;
+        int renderId;
+
+        IEnumerator Start()
+        {
+            probe = probe ?? this.GetComponent<ReflectionProbe>();
+
+            // Example: render every 2 seconds (replace with event-driven requests in production)
+            while( true )
+            {
+                // Kick a time-sliced render into targetRT (or null to update probe's default)
+                renderId = probe.RenderProbe( targetRT );
+                // Wait until it's finished (poll)
+                while( !probe.IsFinishedRendering( renderId ) )
+                    yield return null;
+
+                Cubemap cube = probe.texture as Cubemap; // TODO - bake a custom cubemap with full control instead of this.
+                if( cube != null )
+                {
+                    SphericalHarmonicsL2 sh = ComputeSHFromCubemap( cube, 512 );
+
+                    // reflection probe (high freq).
+                    RenderSettings.customReflectionTexture = cube;
+                    // light probe (low freq)
+                    RenderSettings.ambientProbe = sh;
+                }
+            }
+        }
+
+        // Placeholder -- you need an implementation that samples the cubemap. Could be CPU or GPU compute.
+        private SphericalHarmonicsL2 ComputeSHFromCubemap( Cubemap cubemap, int sampleCount )
+        {
+            SphericalHarmonicsL2 sh = new SphericalHarmonicsL2();
+            sh.Clear();
+
+            if( cubemap == null ) return sh;
+
+            float[] f, s;
+            float fWtSum = 0;
+            foreach( face in cubemap )
+            {
+                foreach( texelXY in face.texels )
+                {
+                    float fTmp = 1.0f + u ^ 2.0f + v ^ 2.0f;
+                    float fWt = 4.0f / (Mathf.Sqrt( fTmp ) * fTmp);
+                    sh.Evaluate( texel, s );
+                    f += face.getPixel(texelXY) * fWt * s; // vector
+                }
+                fWtSum += fWt;
+            }
+            f *= 4 * Mathf.PI / fWtSum; // area of spher
+
+            return sh;
+        }
+    }
+}
